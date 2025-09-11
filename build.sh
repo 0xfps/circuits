e3a06949b968d6cc593d25e569ff72bb86064f56
0xfps <anthony.nnaemeka.umeh@gmail.com>
2025-09-11 08:32:20 +0100

    feat: attach key gen from fifteenfigures.

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
+rm -rf node_modules
 npm i
 cd node_modules/keccak256-circom
 npm i
 
-circom main.circom
+circom main.circom --wasm --r1cs -l node_modules
--- tests/constants.ts
+++ tests/constants.ts
@@ -1,16 +1,24 @@
-import { standardizeToPoseidon } from "@fifteenfigures/mini-merkle-tree"
+import { generatekeys, standardizeToPoseidon } from "@fifteenfigures/mini-merkle-tree"
 import { keccak256 } from "ethers"
 import { strToHex } from "hexyjs"
+import RandomString from "randomstring"
 
 export const prime = 21888242871839275222246405745257275088548364400416034343698204186575808495617n
 
-export const secretKey = "mysecretkeythere"
+export const secretKey = RandomString.generate({ length: 16, charset: "alphanumeric"})
 export const address = "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4"
 export const amount = 100
 
-export const withdrawalKey = "0x5cf20babf05b9104224af49a1a7ac84d3a93febcac1d1897af803fcc8892242eb7ce8c93780B492a365d6B44b079b46816F090780000000000000000000000000000000000000000000000000000000000000005"
-export const depositKey = "0xec3a829a0d177d656b4b2bafa101e4f7a47c17db21202c81e2d943d344c6e4beb7ce8c93780B492a365d6B44b079b46816F090780000000000000000000000000000000000000000000000000000000000000005"
+const keys = generatekeys(address, BigInt(amount), secretKey)
+export const withdrawalKey = keys.withdrawalKey
+export const depositKey = keys.depositKey
 export const withdrawalKeyConcat = `${withdrawalKey}${strToHex(secretKey)}`
+
+console.log({
+    secretKey,
+    withdrawalKey,
+    depositKey
+})
 
 export const depositkeyHash = keccak256(depositKey)
 export const depositkeyHashInTree = standardizeToPoseidon(depositKey)
